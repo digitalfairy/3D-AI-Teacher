ff7aa1534a561045f9c5e2a208e1095083855a6d
Anita <jelicanita7@gmail.com>
2025-07-24 12:29:30 +0200

    React Three Fiber w/Next.js
    Positioning, scaling 3d models.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,46 @@
 "use client"
-import { Box, OrbitControls } from "@react-three/drei";
+import { CameraControls, Environment, Gltf } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Teacher } from "./Teacher";
+import { degToRad } from "three/src/math/MathUtils";
 
 export const Experience = () => {
     return (
         <>
-            <Canvas>
-                <OrbitControls />
-                <Box>
-                    <meshNormalMaterial />
-                </Box>
+            <Canvas 
+            camera={{
+                position: [0, 0, 0.0001],
+            }}>
+                <CameraControls />
+                <Environment preset="sunset" />
+                <ambientLight intensity={0.8} color="pink" />
+                <Teacher 
+                    teacher={"Miguel"} 
+                    position={[-0.95, -0.25, -3]} 
+                    scale={2.8} 
+                    rotation-y={degToRad(20)}
+                />
+                <Gltf src="/models/basic_classroom.glb" position={[0.2, -1.3, -2]} rotation-y={-1.5} />
             </Canvas>
         </>
+    );
+};
+
+const  CameraManager = () => {
+    return (
+        <CameraControls
+            minZoom={1}
+            maxZoom={3}
+            polarRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
+            azimuthRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
+            mouseButtons={{
+                left: 1,  //ACTION.ROTATE
+                wheel: 16, //ACTION.ZOOM
+            }}
+            touches={{
+                one: 32, // ACTION_TOUCH_ROTATE
+                two: 512,  // ACTION.TOUCH_ZOOM
+            }}
+        />
     );
 };
