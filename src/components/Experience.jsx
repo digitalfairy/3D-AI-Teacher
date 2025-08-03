b4b3c9fa2534003bd0064b1fb34dc76b1581ddbf
Anita <jelicanita7@gmail.com>
2025-08-03 17:04:54 +0200

    3D Html UI
    The board layout.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,19 +1,29 @@
 "use client"
-import { CameraControls, Environment, Gltf } from "@react-three/drei";
+import { CameraControls, Environment, Gltf, Html } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Teacher } from "./Teacher";
 import { degToRad } from "three/src/math/MathUtils";
+import { TypingBox } from "./TypingBox";
+import { MessagesList } from "./MessagesList";
+import { BoardSettings } from "./BoardSettings";
 
 export const Experience = () => {
     return (
         <>
+            <div className="z-10 md:justify-center fixed bottom-4 left-4 right-4 flex gap-3 flex-wrap justify-stretch">
+                <TypingBox />
+            </div>
             <Canvas 
             camera={{
                 position: [0, 0, 0.0001],
             }}>
-                <CameraControls />
+                <CameraManager />
                 <Environment preset="sunset" />
                 <ambientLight intensity={0.8} color="pink" />
+                <Html position={[0.22, 0.192, -3]} transform distanceFactor={0.5}>
+                    <MessagesList />
+                    <BoardSettings />
+                </Html>
                 <Teacher 
                     teacher={"Elias"} 
                     position={[-0.95, -0.25, -3]} 
@@ -26,7 +36,7 @@ export const Experience = () => {
     );
 };
 
-const  CameraManager = () => {
+const CameraManager = () => {
     return (
         <CameraControls
             minZoom={1}
